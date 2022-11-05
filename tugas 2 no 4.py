22e101f61da7b57409da581cbd85d0b7dff9b4b3
M. Priambodo Satrio W <117236289+saatriow@users.noreply.github.com>
2022-11-05 15:09:06 +0700

    Add files via upload
    No 4

---
+++ tugas 2 no 4.py
@@ -0,0 +1,10 @@
+#buat program pola n x n, bintang dan tanda pagar dengan jumlah n(integer) 
+n = int(input("Masukan nilai N : "))
+a = n+2
+b = 1
+print("*  "*a)
+
+for j in range(n):
+    print("*"," # "*n,"*")
+    
+print("*  "*a)
