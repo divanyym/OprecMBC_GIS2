b6377e040dcd6745c8ca12209e9f3105fa0fdc90
M. Priambodo Satrio W <117236289+saatriow@users.noreply.github.com>
2022-11-05 15:59:02 +0700

    Add files via upload

---
+++ Tugas 2 no 1.py
@@ -0,0 +1,11 @@
+hari=input("Masukkan nama hari : ")
+if (hari == "senin" ):
+    print("Seragam anda hari ini adalah merah")
+elif (hari == "selasa" and "rabu"):
+    print("Seragam anda hari ini adalah putih")
+elif (hari == "kamis" and "sabtu"):
+    print("Seragam kamu hari ini adalah baju bebas")
+elif (hari == "jumat"):
+    print("Seragam kamu hari ini adlah baju batik")
+else:
+    print("maaf kamu salah memasukan nama hari (ex:senin,jumat)")
