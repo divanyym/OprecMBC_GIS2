f377f50381bea53be5ccf3a3c182d4656ddfd77a
M. Priambodo Satrio W <117236289+saatriow@users.noreply.github.com>
2022-11-05 21:28:34 +0700

    Update Tugas 2 no 3.py

--- Tugas 2 no 3.py
+++ Tugas 2 no 3.py
@@ -1,15 +1,11 @@
 hari=input("Masukkan nama hari : ")
 if (hari == "senin" ):
     print("Seragam anda hari ini adalah merah")
-elif (hari == "selasa"):
+elif (hari == "selasa" or "rabu"):
     print("Seragam anda hari ini adalah putih")
-elif (hari == "rabu"):
-    print("Seragam anda hari ini adalah putih")
-elif (hari == "kamis"):
+elif (hari == "kamis" or "sabtu"):
     print("Seragam kamu hari ini adalah baju bebas")
 elif (hari == "jumat"):
     print("Seragam kamu hari ini adlah baju batik")
-elif (hari == "sabtu"):
-    print("Seragam kamu hari ini adalah baju bebas")
 else:
     print("maaf kamu salah memasukan nama hari (ex:senin,jumat)")
