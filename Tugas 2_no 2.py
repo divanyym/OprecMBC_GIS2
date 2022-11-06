04de3dadc8ecbf39a61dd81da6d5b3c6a8c379fc
divanyym <117184423+divanyym@users.noreply.github.com>
2022-11-06 13:04:36 +0700

    Update Tugas 2_no 2.py

--- Tugas 2_no 2.py
+++ Tugas 2_no 2.py
@@ -1,22 +1,27 @@
 print("========== SELAMAT DATANG DI GIS 2 MART ==========")
-totalbelanja = float(input('Masukkan Total Belanja Anda : '))
+totalbelanja = int(input('Masukkan Total Belanja Anda : '))
 member = input ("Apakah Anda Member ? Silahkan Jawab y atau t: ")
 if (member == "y"):
     if (totalbelanja >= 1000000):
         diskon = 8
-    elif (totalbelanja >= 500000 and totalbelanja < 1000000):
+        print("Selamat anda mendapatkan diskon sebesar ", diskon, "%")
+
+    elif (totalbelanja < 1000000 or totalbelanja >= 500000):
         diskon = 7
-    else : 
-        diskon = 5
-    print("Selamat anda mendapatkan diskon sebesar ", diskon, "%")
+        print("Selamat anda mendapatkan diskon sebesar ", diskon, "%")
+
+    else:
+        print("Nominal yang anda masukkan salah ex:(1000000, 500000)")
+
 elif (member == "t"):
     if (totalbelanja >= 1000000):
         diskon = 3
-    elif(totalbelanja >= 500000 and totalbelanja < 1000000):
+        print("Selamat anda mendapatkan diskon sebesar ", diskon, "%")
+
+    elif(totalbelanja >= 500000 or totalbelanja < 1000000):
         diskon = 2
+        print("Selamat anda mendapatkan diskon sebesar ", diskon, "%")
+
     else:
-        diskon = 0
-    print("Terimakasih telah berbelanja, total diskon Anda adalah ", diskon, "%")
-else:
-    print("Maaf input yang anda masukkan salah")
+        print("Nominal yang anda masukkan salah ex:(1000000, 500000)")
     
