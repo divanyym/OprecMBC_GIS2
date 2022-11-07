368805f4e7512a07ea266d52c630020482ca52f0
divanyym <117184423+divanyym@users.noreply.github.com>
2022-11-07 10:21:39 +0700

    Delete tugas 2 no 4.py

--- tugas 2 no 4.py
+++
@@ -1,9 +0,0 @@
-#buat program pola n x n, bintang dan tanda pagar dengan jumlah n(integer) 
-n = int(input("Masukan nilai N : "))
-a = n+2
-print("*  "*a)
-
-for j in range(n):
-    print("*"," # "*n,"*")
-    
-print("*  "*a)
